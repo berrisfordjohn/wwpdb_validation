1bcbd25c08b1f6a7bde57ab8493569ffde089ddc
jmb <jmb@ebi.ac.uk>
2019-12-22 10:35:03 +0000

    corrected import of tests added tox.ini added email address

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wwPDB validation',
+    name='wwPDB validation tests',
     version='0.1',
     url='https://github.com/berrisfordjohn/wwpdb_validation',
     author='John Berrisford',
+    author_email='berrisfordjohn@gmail.com',
     test_suite='tests',
     zip_safe=True,
     packages=['wwpdb_validation'],
-    # dependency_links=['https://github.com/project-gemmi/gemmi/tarball/master#egg=gemmi'],
     install_requires=['requests',
                       'onedep_api>=0.15'
                       ],
--- tests/test_wwpdb_validation_api.py
+++ tests/test_wwpdb_validation_api.py
@@ -1,5 +1,5 @@
 import unittest
-from tests.access_test_files import TestFiles
+# from tests.access_test_files import TestFiles
 import tempfile
 import logging
 import os
@@ -7,11 +7,31 @@ import shutil
 
 import wwpdb_validation.wwpdb_validation_api as wwpdb_validation_api
 
-
 logger = logging.getLogger()
 FORMAT = "%(filename)s - %(funcName)s - %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
+
+
+class TestFiles:
+
+    def __init__(self):
+        self.FILE_ROOT = os.path.dirname(os.path.realpath(__file__))
+        self.path = os.path.dirname(os.path.join(self.FILE_ROOT, '..', '..'))
+        self.test_data = os.path.join(self.path, 'test_data')
+
+        self.xml = None
+        self.cif = None
+        self.sf = None
+
+    def pdb3zt9(self):
+        self.xml = os.path.join(self.test_data, "3zt9_validation.xml")
+        self.cif = os.path.join(self.test_data, 'pdb3zt9_refmac1_output.cif')
+        self.sf = os.path.join(self.test_data, 'r3zt9sf.ent')
+
+    def pdb1cbs(self):
+        self.xml = os.path.join(self.test_data, "1cbs_validation.xml")
+
 
 class TestValidationReportGeneration(unittest.TestCase):
 
@@ -24,22 +44,22 @@ class TestValidationReportGeneration(unittest.TestCase):
         output_pdf_file = os.path.join(test_dir, 'output.pdf')
         output_xml_file = os.path.join(test_dir, 'output.xml')
         output_log_file = os.path.join(test_dir, 'output.log')
-        #output_cif_file = os.path.join(test_dir, 'output.cif')
+        # output_cif_file = os.path.join(test_dir, 'output.cif')
         output_svg_file = os.path.join(test_dir, 'output.svg')
         output_2fofc_file = os.path.join(test_dir, 'output_2fofc')
         output_fofc_file = os.path.join(test_dir, 'output_fofc')
         self.test_files.pdb3zt9()
         worked = wwpdb_validation_api.run_validation_api(
-            model_file_path = self.test_files.cif,
-            structure_factors= self.test_files.sf,
+            model_file_path=self.test_files.cif,
+            structure_factors=self.test_files.sf,
             output_pdf_file_name=output_pdf_file,
             output_xml_file_name=output_xml_file,
             output_log_file_name=output_log_file,
             output_svg_file_name=output_svg_file,
-            #output_2fofc_file_name=output_2fofc_file,
-            #output_fofc_file_name=output_fofc_file,
+            # output_2fofc_file_name=output_2fofc_file,
+            # output_fofc_file_name=output_fofc_file,
             api_input_url="https://validate-pdbe.wwpdb.org/"
-            )
+        )
         self.assertTrue(worked)
         shutil.rmtree(test_dir)
 
